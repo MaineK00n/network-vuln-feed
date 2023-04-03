2892efae4bb78cafc224bddbdd90b2331468727c
MaineK00n <mainek00n.1229@gmail.com>
2023-04-04 02:36:33 +0900

    feat: template, discover, finish cmd for fortinet

---
+++ pkg/template/nec/nec_test.go
@@ -0,0 +1,19 @@
+package nec
+
+import "testing"
+
+func TestTemplate(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Template(); got != tt.want {
+				t.Errorf("Template() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
