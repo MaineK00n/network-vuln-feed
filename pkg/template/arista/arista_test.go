fbfd6d2d587292917458e3914b32bbf1f3571f32
MaineK00n <mainek00n.1229@gmail.com>
2023-04-04 02:36:33 +0900

    feat: template, discover, finish cmd for fortinet

---
+++ pkg/template/arista/arista_test.go
@@ -0,0 +1,19 @@
+package arista
+
+import "testing"
+
+func TestTemplate(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Template(); got != tt.want {
+				t.Errorf("Template() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
