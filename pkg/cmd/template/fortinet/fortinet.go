efc4cb903ad67dbb34b4dbac3ce5306ec3748bd3
MaineK00n <mainek00n.1229@gmail.com>
2023-04-06 04:23:08 +0900

    feat(cmd/template): add cmd template

--- pkg/cmd/template/fortinet/fortinet.go
+++ pkg/cmd/template/fortinet/fortinet.go
@@ -1,6 +1,9 @@
 package fortinet
 
 import (
+	"fmt"
+
+	"github.com/MaineK00n/network-vuln-feed/pkg/template/fortinet"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -11,8 +14,13 @@ func NewCmdFortinet() *cobra.Command {
 		Short:   "create a template for a Vuls format advisory from fortinet CVRF",
 		Example: "$ network-vuln-feed template fortinet FG-IR-23-001_cvrf.xml",
 		Args:    cobra.ExactArgs(1),
-		RunE: func(_ *cobra.Command, _ []string) error {
-			return errors.New("not implemented")
+		RunE: func(_ *cobra.Command, args []string) error {
+			s, err := fortinet.Template(args[0])
+			if err != nil {
+				return errors.Wrap(err, "failed to generate vuls format security advisory")
+			}
+			fmt.Println(s)
+			return nil
 		},
 	}
 
